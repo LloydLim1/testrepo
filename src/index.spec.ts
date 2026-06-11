08d64d832f19b2d1d8b04a57b25b44ef5230bedf
LloydLim1 <111641482+LloydLim1@users.noreply.github.com>
2026-06-11 14:52:57 +0800

    chore: initialize project scaffold

---
+++ src/index.spec.ts
@@ -0,0 +1,7 @@
+import { SERVICE_NAME } from './index';
+
+describe('testrepo-backend', () => {
+  it('should export SERVICE_NAME', () => {
+    expect(SERVICE_NAME).toBe('testrepo-backend');
+  });
+});
